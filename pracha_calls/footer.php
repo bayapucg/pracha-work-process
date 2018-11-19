fbd6165dedd54cb2eb956511d03f328de48ff4ae
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-11-19 14:13:53 +0530

    commit

---
+++ pracha_calls/footer.php
@@ -0,0 +1,17 @@
+    </div><!-- /#right-panel -->
+
+    <!-- Right Panel -->
+    <script src="assets/js/jquery-2.1.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+    
+    <script src="assets/js/datatables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap.min.js"></script>
+    <script src="assets/js/datatables-init.js"></script>
+
+    <script src="assets/js/Chart.bundle.js"></script>
+    <script src="assets/js/chartjs-init.js"></script>
+
+</body>
+</html>
---
+++ pracha_calls/sidebar.php
@@ -0,0 +1,37 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+
+        <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="index.php">Prachatech</a>
+            <a class="navbar-brand hidden" href="index.php"><img src="assets/img/fav.png" alt="Prachatech"></a>
+        </div>
+
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                </li>
+                <li>
+                    <a href="start_campaign.php"> <i class="menu-icon fa fa-dashboard"></i>Start Campaign </a>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="menu-icon fa fa-users"></i>Group</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-angle-right"></i><a href="add_group.php">Add</a></li>
+                        <li><i class="menu-icon fa fa-angle-right"></i><a href="group_list.php">List</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="menu-icon fa fa-file"></i>SMS-Email Templates</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-angle-right"></i><a href="add_sms_email_template.php">Add</a></li>
+                        <li><i class="menu-icon fa fa-angle-right"></i><a href="sms_email_template_list.php">List</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside>
